37bd2f5e7da12e0b5b2462bab0f93c94120af1eb
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2025-10-24 18:30:44 -0300

    agregado de url de render en enviroments angular

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000'  // URL de la API en el entorno de desarrollo
+    apiUrl: 'https://sti-app-desarrollo-workflow.onrender.com'  // URL de la API en el entorno de desarrollo
   };
   
